3f8af001135e79792eee22bc04d4ba9f79114fa9
Valentin Charbonnier <valentinbreiz@gmail.com>
2023-08-22 10:32:23 +0200

    🔨 Update docker compose template

--- src/firebase.py
+++ src/firebase.py
@@ -1,10 +1,10 @@
-import requests
 import grpc
-import protos
+from protos.user_pb2_grpc import UserStub
+from protos.user_pb2 import SignInSignUpRequest
 
 def create_account(username, password):
     with grpc.insecure_channel('localhost:8080') as channel:
-        stub = protos.user_pb2_grpc.UserStub(channel)
-        request = protos.user_pb2.SignInSignUpRequest(email=username, password=password)
+        stub = UserStub(channel)
+        request = SignInSignUpRequest(email=username, password=password)
         response = stub.SignUp(request)
         print(response.message)
